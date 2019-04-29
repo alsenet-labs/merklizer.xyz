64189657f11322b8bd3a511576312f317adc01f7
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2019-04-29 17:41:19 +0200

    add comments to saf-build.sh

--- safe-build.sh
+++ safe-build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 set -x
+
+# run a nodejs container to create the merklizer.xyz docker volume and to
+# change the mountpoint ownership
 docker run --rm --name merklizer.xyz --mount source=merklizer.xyz,destination=/home/nodejs/src/merklizer.xyz -u root nodejs chown nodejs.nodejs src/merklizer.xyz
+
+# create an nodejs container named 'merklizer.xyz' to clone and build
+# merklizer.xyz in the docker volume named merklizer.xyz
 docker run --name merklizer.xyz --mount source=merklizer.xyz,destination=/home/nodejs/src/merklizer.xyz -i nodejs << 'EOF'
 set -x
 cd src/merklizer.xyz
