6feec4e6a7a77b31aad460c523b47d58ec3c9858
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2019-05-23 07:43:03 +0200

    use current remote origin for safe-build

--- safe-build.sh
+++ safe-build.sh
@@ -2,15 +2,17 @@
 set -e
 set -x
 
-# run a nodejs container to create the merklizer.xyz docker volume and to
-# change the mountpoint ownership
-docker run --rm --name merklizer.xyz --mount source=merklizer.xyz,destination=/home/nodejs/src/merklizer.xyz -u root nodejs chown nodejs.nodejs src/merklizer.xyz
+PACKAGE=$1
 
-# create an nodejs container named 'merklizer.xyz' to clone and build
-# merklizer.xyz in the docker volume named merklizer.xyz
-docker run --name merklizer.xyz --mount source=merklizer.xyz,destination=/home/nodejs/src/merklizer.xyz -i nodejs << 'EOF'
+# run a nodejs container to create the docker volume named $PACKAGE
+# and to change the mountpoint ownership
+docker run --rm --name $PACKAGE --mount source=$PACKAGE,destination=/home/nodejs/src/$PACKAGE -u root nodejs chown nodejs.nodejs src/$PACKAGE
+
+# create an nodejs container named $PACKAGE to clone and build
+# $PACKAGE in the docker volume named $PACKAGE
+docker run --name $PACKAGE --mount source=$PACKAGE,destination=/home/nodejs/src/$PACKAGE -i nodejs << EOF
 set -x
-cd src/merklizer.xyz
-[ -d .git ] || git clone --recursive https://github.com/alsenet-labs/merklizer.xyz .
-make unsafe-build && echo "The package is available in /var/lib/docker/volumes/merklizer.xyz/_data/dist"
+cd src/$PACKAGE
+git clone --recursive $(git config --get remote.origin.url) .
+make unsafe-build && echo 'The package is available in /var/lib/docker/volumes/$PACKAGE/_data/dist'
 EOF
